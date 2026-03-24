2d86f58e1f5abe1143e2c64a5494a186f3e16cb7
August Damm Lindbæk <augustdl@stud.ntnu.no>
2026-03-24 11:53:46 +0000

    Feat: lobby, doesNotUnderstand and parent management.

--- Prototypes.jl
+++ Prototypes.jl
@@ -4,20 +4,21 @@ mutable struct Object
     parents::Vector{Object}
 end
 
+const lobby = Object(Dict{Symbol, Any}(), Object[])
+
 function object(; slots...)
-    Object(Dict(slots), Object[])
+    Object(Dict(slots), [lobby])
 end
 
 function get_slot(obj, name)
-    if haskey(obj.slots, name)
+    if has_own_slot(obj, name)
         return obj.slots[name]
     end    
 
     for parent in obj.parents
-        try
+        if has_slot(parent, name)
             return get_slot(parent, name)
-        catch
-        end  
+        end
     end
     
     error("Slot $name not found")
@@ -27,25 +28,67 @@ function set_slot!(obj, name, value)
     obj.slots[name] = value
 end
 
+function has_own_slot(obj, name)
+    return haskey(obj.slots, name)
+end
+
+function has_slot(obj, name)
+    if has_own_slot(obj, name)
+        return true
+    end
+
+    for parent in obj.parents
+        if has_slot(parent, name)
+            return true
+        end
+    end 
+
+    return false
+end
+
+function own_slots(obj)
+    return collect(keys(obj.slots))
+end
+
+function get_parents(obj)
+    return obj.parents
+end
+
+function add_parent!(obj, parent)
+    push!(obj.parents, parent)
+end
+
+function remove_parent!(obj, parent)
+    filter!(p -> p !== parent, obj.parents)
+end
+
+function set_parents!(obj, parents)
+    obj.parents = parents
+end 
 
 function clone(proto; slots...)
     new_obj = Object(Dict(slots), [proto])
     return new_obj
 end
 
+set_slot!(lobby, :doesNotUnderstand,
+    (self, msg, args...) -> error("Object does not understand message $msg")
+)
+
 function send(obj, msg, args...)
-    value = get_slot(obj, msg)
-    if value isa Function
-        return value(obj, args...)
-    else
-        return value
+    if has_slot(obj, msg)
+        value = get_slot(obj, msg)
+        if value isa Function
+            return value(obj, args...)
+        else
+            return value
+        end
+    else 
+        dnu = get_slot(obj, :doesNotUnderstand)
+        return dnu(obj, msg, args...)
     end
 end
 
 
-dog = object(
-    name="Rex",
-    speak=(self) -> "$(get_slot(self, :name)) says woof"
-)
 
-println(send(dog, :speak))
+
